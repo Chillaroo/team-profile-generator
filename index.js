8dc349f7956ac021a71768ac729ee131c01e14a7
Rachel <rtc145@gmail.com>
2021-08-09 11:44:25 -0400

    created function to generate divs with employee info

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const inquirer = require("inquirer");
+const Manager = require("./lib/Manager.js");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
 var newEmployee = "manager";
+var newTeam = [];
+var divs = [];
 
 function getQuestions(employee) {
 
@@ -11,7 +16,7 @@ function getQuestions(employee) {
     {
         type: "input",
         message: `What is the ${employee}'s employee id?`,
-        name: "name"
+        name: "id"
     },
     {
         type: "input",
@@ -29,7 +34,7 @@ function getQuestions(employee) {
             name: "add",
             message: "Which type of employee would you like to add next?",
             choices: ["Engineer", "Intern", "I'm done"]
-    }];
+        }];
     let engineerQuestions = [{
         type: "input",
         message: "What is the engineer's github username?",
@@ -68,18 +73,9 @@ inquirer
 
 async function setUserInput(res) {
 
+    newTeam.push(new Manager(res.name, res.id, res.email, res.office));
+
     while (newEmployee === "manager") {
-        if (res.add === "Intern") {
-            newEmployee = "intern";
-        } else if (res.add === "Engineer") {
-            newEmployee = "engineer";
-        } else {
-            employee = "none";
-        }
-    }
-    while ((newEmployee === "engineer")||(newEmployee === "intern")) {
-        res = await inquirer
-            .prompt(getQuestions(newEmployee))
         if (res.add === "Intern") {
             newEmployee = "intern";
         } else if (res.add === "Engineer") {
@@ -88,5 +84,71 @@ async function setUserInput(res) {
             newEmployee = "none";
         }
     }
+    while ((newEmployee === "engineer") || (newEmployee === "intern")) {
+        res = await inquirer
+            .prompt(getQuestions(newEmployee))
+        if (newEmployee === "intern") {
+            newTeam.push(new Intern(res.name, res.id, res.email, res.school));
+        } else if (newEmployee === "engineer") {
+            newTeam.push(new Engineer(res.name, res.id, res.email, res.github));
+        }
+        if (res.add === "Intern") {
+            newEmployee = "intern";
+        } else if (res.add === "Engineer") {
+            newEmployee = "engineer";
+        } else {
+            newEmployee = "none";
+        }
+    }
+    let divArray = generateDiv(newTeam);
+    console.log(divArray);
 };
+
+function generateDiv(newTeam) {
+
+    let employeeName = " ";
+    let employeeId = " ";
+    let employeeEmail = " ";
+    let employeePosition = " ";
+    let specialCategoryKey = " ";
+    let specialCategoryValue = " ";
+
+    for (var i = 0; i < newTeam.length; i++) {
+
+        employeeName = newTeam[i].name;
+        employeeId = newTeam[i].id;
+        employeeEmail = newTeam[i].email;
+
+        if (newTeam[i].hasOwnProperty('officeNumber')) {
+            employeePosition = "Manager"
+            specialCategoryKey = "Office Number:";
+            specialCategoryValue = newTeam[i].officeNumber;
+        }
+        if (newTeam[i].hasOwnProperty('github')) {
+            employeePosition = "Engineer"
+            specialCategoryKey = "Github";
+            specialCategoryValue = `https://github.com/${newTeam[i].github}`;
+        }
+        if (newTeam[i].hasOwnProperty('school')) {
+            employeePosition = "Intern"
+            specialCategoryKey = "School";
+            specialCategoryValue = newTeam[i].school;
+        }
+        let divTemplate = `
+        <div class="card" style="width: 18rem;">
+            <div class="card-body bg-primary text-white">
+                <h5 class="card-title">${employeeName}</h5>
+                <h5 class="card-title">${employeePosition}</h5>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Employee ID: ${employeeId}</li>
+                <li class="list-group-item">Employee Email: ${employeeEmail}</li>
+                <li class="list-group-item">${specialCategoryKey}: ${specialCategoryValue}</li>
+            </ul>
+        </div>
+        `
+        divs.push(divTemplate);
+    }
+    return divs.join(" ");
+}
 
